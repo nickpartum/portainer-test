35a9cd37edb0b9660c55ef6d2de8ff0b9dfb4cd2
cooper <nickpartum@gmail.com>
2024-12-12 19:32:17 +0200

    add prisma engine

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const port = 3000
 
 app.get("/", function(req, res) {
     return res.send("Hello World");
 });
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000', "Hello");
-});
+
+(async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`API listening on port ${port}`)
+        })
+
+        await connectToDB();
+
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})()
