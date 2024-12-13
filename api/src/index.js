1dc1138a957b78a4e66df44c04e4d6116fd9eefa
mykola.pron <mykola.pron.dev@gmail.com>
2024-12-13 19:53:47 +0200

    added fix

--- api/src/index.js
+++ api/src/index.js
@@ -2,16 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const router = require("./routes");
+const {Sequelize} = require("sequelize");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const sequelize = new Sequelize(process.env.DATABASE_URL) // Example for postgres
+
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api/v1', router);
+app.use('/', (req, res, next) => {
+    res.json("dsfsdfsd")
+});
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 });
